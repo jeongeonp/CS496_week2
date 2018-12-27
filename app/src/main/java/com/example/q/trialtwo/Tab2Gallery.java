42c0d25bf00ddd64f68050166209acdb806ef23f
Jeongeon Park <patricia021912@gmail.com>
2018-12-27 19:47:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/q/trialtwo/Tab2Gallery.java
@@ -0,0 +1,17 @@
+package com.example.q.trialtwo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Gallery extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2gallery, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/q/trialtwo/Tab3Undecided.java
@@ -0,0 +1,16 @@
+package com.example.q.trialtwo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Undecided extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3undecided, container, false);
+        return rootView;
+    }
+}
